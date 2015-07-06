147cb007bda190a84847845c2ee9db772a6921b4
Fa Kong <konglingfa@yuanxin-inc.com>
2015-07-06 19:08:55 +0800

    3个测试文件

---
+++ JKColorDefine.h
@@ -0,0 +1,41 @@
+//
+//  JKColorDefine.h
+//  JKUtilsProject
+//
+//  Created by Fa Kong on 15/7/6.
+//  Copyright (c) 2015年 JackKong. All rights reserved.
+//
+
+#ifndef JKUtilsProject_JKColorDefine_h
+#define JKUtilsProject_JKColorDefine_h
+
+//#FFFFFF：纯白色，通用。
+//#000000：纯黑色，通用。
+//#FF0000：纯红色，通用。详情页下载链接、访问网络链接。
+//#00FF00：纯绿色，通用。
+//#0000FF：纯蓝色，通用。详情页下载链接、访问网络链接点击状态颜色。
+
+#define kRedColor 0xFF3B30 
+#define kWhiteColor 0xffffff
+#define kBlackTextColor 0x333333
+#define kBlackText999Color 0x999999
+#define kBlackText333Color 0x333333
+#define kGrayColor 0xDADADA
+#define kGrayBgColor 0xF6F6F6
+#define kGrayHeaderColor 0xebebeb
+#define kNavBgColor 0xf6f6f7
+#define kNavBottomLineColor 0xaaaaaa
+//#E1E1E1
+//#EBEBEB
+#define kSelectBgColor 0xf2f2f2
+#define kGrayTextColor 0x666666
+#define kBlueColor 0x00aeff
+#define kOrangeColor 0xfc7703
+#define kBorderColor 0xdfdfdf
+#define kBgViewColor 0xf7f7f7
+#define kCellSelectBgColor 0xf8f8f8
+#define k757575Color 0x757575
+#define kSeparaterLineColor 0xdedede
+#define kCellLineViewColor 0xd7d7d7
+
+#endif
---
+++ JKDefine.h
@@ -0,0 +1,15 @@
+//
+//  JKDefine.h
+//  JKUtilsProject
+//
+//  Created by Fa Kong on 15/7/6.
+//  Copyright (c) 2015年 JackKong. All rights reserved.
+//
+
+#ifndef JKUtilsProject_JKDefine_h
+#define JKUtilsProject_JKDefine_h
+
+#import "KKDColorDefine.h"
+#import "KKDToolsDefine.h"
+
+#endif
